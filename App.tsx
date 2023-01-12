eccd57d4ae14c6ea8ae3e6eea4587ca8ee07a582
mackenzie <mackenzie@yourmoms>
2023-01-11 20:19:31 -0600

    Weird

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import {Button, Text, View} from 'react-native';
 
 // Navigation
 import {DarkTheme, NavigationContainer} from '@react-navigation/native';
+import {CommonActions, NavigationAction} from '@react-navigation/routers';
 import {createStackNavigator} from '@react-navigation/stack';
 
 function SomeOtherScreen() {
@@ -21,15 +22,66 @@ function SomeOtherScreen() {
 
 function SomeScreen({navigation}) {
   const [someValue, setSomeValue] = useState(1);
+  console.log('Some Screen has been rendered');
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>{someValue}</Text>
-      <Button title={'Increment'} onPress={() => setSomeValue(someValue + 1)} />
-      <Button
-        title={'Go to other screen'}
-        onPress={() => navigation.push('Some Other Screen')}
-      />
+      <View>
+        <Text
+          style={{
+            fontSize: 40,
+          }}>
+          Value
+        </Text>
+        <Text style={{fontSize: 30, textAlign: 'center'}}>{someValue}</Text>
+        <Button
+          title={'Increment'}
+          onPress={() => setSomeValue(someValue + 1)}
+        />
+      </View>
+
+      <View>
+        <Text style={{fontSize: 40}}>Re-renders, destroying state</Text>
+        <Button title={'Push'} onPress={() => navigation.push('Some Screen')} />
+        <Button
+          title={'Replace'}
+          onPress={() => navigation.replace('Some Screen')}
+        />
+        <Button
+          title={'Navigate'}
+          onPress={() =>
+            navigation.dispatch(
+              CommonActions.navigate({name: 'Some Other Screen'}),
+            )
+          }
+        />
+        <Button
+          title={'Reset'}
+          onPress={() =>
+            navigation.reset({index: 1, routes: [{name: 'Some Screen'}]})
+          }
+        />
+      </View>
+
+      <View>
+        <Text style={{fontSize: 40}}>Re-renders, preserving state</Text>
+        <Button
+          title={'Set Params'}
+          onPress={() =>
+            navigation.dispatch(CommonActions.setParams({user: 'Wojtek'}))
+          }
+        />
+      </View>
+
+      <View>
+        <Text style={{fontSize: 40}}>Does not re-render</Text>
+        <Button
+          title={'Navigate To Self'}
+          onPress={() =>
+            navigation.dispatch(CommonActions.navigate({name: 'Some Screen'}))
+          }
+        />
+      </View>
     </View>
   );
 }
